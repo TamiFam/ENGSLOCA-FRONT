0406e2df5710aa9f2eaa4ce29feeceaf96bbc846
TamiFam <ilya.innain@mail.ru>
2026-06-10 14:24:52 +0300

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd())
 
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-        }
-      }
-    }
-  }
-})
+//   return {
+//     plugins: [react(), tailwindcss()],
+//     server: {
+//       proxy: {
+//         '/api': {
+//           target: env.VITE_API_BASE_URL,
+//           changeOrigin: true,
+//           secure: false,
+//         }
+//       }
+//     }
+//   }
+// })
